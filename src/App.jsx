1080130b54a61853f6f8f4a5ce4c5ca8af6fda6e
philomina2516 <philomina.khristy.sutraanalytics@gmail.com>
2025-02-04 11:41:55 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import Header from "./components/Header";
-import Search from "./components/Search/Search";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,8 +13,6 @@ function App() {
         <Header />
         <Routes>
           <Route path={"/"} exacts={true} element={<Home />}></Route>
-          <Route path={"/search"} element={<Search />}></Route>
-          {/* <Route path={"/"} exacts={true} element={<Search />}></Route> */}
         </Routes>
       </BrowserRouter>
     </>
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div className='searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[5px]'>
+    <div className='searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[5px] relative p-2'>
       <input type='text' placeholder='search products...' className='w-full h-[35px]
       focus:outline-none bg-inherit'/>
     </div>
