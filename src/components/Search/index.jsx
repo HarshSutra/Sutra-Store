bc88f6f56298b10b08da2d03b7c95094e8095e8f
philomina2516 <philomina.khristy.sutraanalytics@gmail.com>
2025-02-04 11:01:07 +0530

    commit

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div className='searchBox w-[100%] h-[50px] bg-[#e5e5e5]'>
+    <div className='searchBox w-[100%] h-[50px] bg-[#e5e5e5] rounded-[5px]'>
 
     </div>
   )
